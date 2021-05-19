9b7ab806247712c434313200b92ab470a4b7c527
Andrea Cantagallo <andrea_cantagallo@libero.it>
2021-05-19 20:32:43 +0200

    Set up commit

---
+++ src/main/java/it/univaq/mwt/myhealth/MyHealthApplication.java
@@ -0,0 +1,13 @@
+package it.univaq.mwt.myhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/univaq/mwt/myhealth/MyHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.mwt.myhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
