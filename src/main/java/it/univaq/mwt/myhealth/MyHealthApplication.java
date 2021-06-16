702d83faa32b973fd125d972c189b9f6770d9803
umbeboss987 <www.umbe_96@yahoo.it>
2021-06-16 16:08:11 +0200

    Templates structure

--- src/main/java/it/univaq/mwt/myhealth/MyHealthApplication.java
+++ src/main/java/it/univaq/mwt/myhealth/MyHealthApplication.java
@@ -2,7 +2,8 @@ package it.univaq.mwt.myhealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
 @SpringBootApplication
 public class MyHealthApplication {
 
--- src/main/java/it/univaq/mwt/myhealth/presentation/TestController.java
+++ src/main/java/it/univaq/mwt/myhealth/presentation/TestController.java
@@ -3,6 +3,7 @@ package it.univaq.mwt.myhealth.presentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.univaq.mwt.myhealth.business.UserService;
@@ -15,7 +16,7 @@ public class TestController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/test-view")
+	@GetMapping(value="/test-view")
 	public void all() {
 		User user = new User();
 		user.setUsername("admin");
@@ -24,10 +25,42 @@ public class TestController {
 		userService.save(user);
 	}
 	
-	@GetMapping("/pippo")
-	public void pippo() {}
 	
-	@GetMapping("/page-about")
-	public void about() {}
+	@GetMapping(value="/about")
+	public String about (){
+		return "about";
+	}
 	
+	
+	
+	@GetMapping(value="/doctor")
+	public String doctorView (){
+		return "doctor";
+	}
+	
+	@GetMapping(value="/contact")
+	public String contactUs (){
+		return "contactUs";
+	}
+	
+	@GetMapping(value="/login")
+	public String login (){
+		return "login";
+	}
+	
+
+	@GetMapping(value="/sign-up")
+	public String signUp (){
+		return "signUp";
+	}
+	
+	@GetMapping(value="/listing")
+	public String listing (){
+		return "listings";
+	}
+	
+	@GetMapping(value="/home")
+	public String home (){
+		return "home";
+	}
 }
