840259c175ed91a9c307325957b9b8e5bb9122d7
umbeboss987 <www.umbe_96@yahoo.it>
2021-08-05 20:21:14 +0200

    SingleDoctorPage and Mapping

--- src/main/java/it/univaq/mwt/myhealth/presentation/SingleDoctorController.java
+++ src/main/java/it/univaq/mwt/myhealth/presentation/SingleDoctorController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.univaq.mwt.myhealth.business.UserService;
@@ -11,15 +12,16 @@ import it.univaq.mwt.myhealth.business.exceptions.BusinessException;
 
 @Controller
 @RequestMapping("doctors")
-public class DoctorController {
+public class SingleDoctorController {
 	
+
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping(value="")
-	public String Doctors (Model model) throws BusinessException
-	{
-		model.addAttribute("doctors", userService.findUserByRole(2));
-		return "public/doctors"; 
+	@GetMapping("/{name}")
+	public String doctor (Model model, @PathVariable("name") String name) throws BusinessException {		
+		model.addAttribute("doctor", userService.findUserByUsername( name));
+		return "public/singleDoctor";
 	}
+
 }
