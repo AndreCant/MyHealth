b2b46931f96014234e044378fff5eb36144327c8
Andrea Cantagallo <andrea_cantagallo@libero.it>
2021-09-22 20:25:52 +0200

    Update user list page

--- src/main/java/it/univaq/mwt/myhealth/presentation/AdminController.java
+++ src/main/java/it/univaq/mwt/myhealth/presentation/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.univaq.mwt.myhealth.business.BusinessException;
 import it.univaq.mwt.myhealth.business.UserService;
@@ -26,7 +27,14 @@ public class AdminController {
 	}
 	
 	@GetMapping(value="/users")
-	public String users () throws BusinessException{	
+	public String users (Model model) throws BusinessException{	
+		model.addAttribute("users", userService.findAllUsers());
 		return "private/admin/users";
+	}
+	
+	@GetMapping(value="/user/delete")
+	public String deleteUser (@RequestParam("id") Long id, Model model) throws BusinessException{	
+		userService.deleteUser(id);
+		return "redirect:/admin/users";
 	}
 }
