02ef2f9772eae988713e8ecfbb1fdfc09603d6ce
umbeboss987 <www.umbe_96@yahoo.it>
2021-09-30 18:27:20 +0200

    dashboard listings patient

--- src/main/java/it/univaq/mwt/myhealth/presentation/PatientController.java
+++ src/main/java/it/univaq/mwt/myhealth/presentation/PatientController.java
@@ -1,5 +1,7 @@
 package it.univaq.mwt.myhealth.presentation;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import it.univaq.mwt.myhealth.business.BusinessException;
+import it.univaq.mwt.myhealth.business.ReservationService;
 import it.univaq.mwt.myhealth.business.UserService;
+import it.univaq.mwt.myhealth.domain.Reservation;
 import it.univaq.mwt.myhealth.domain.User;
 import org.springframework.validation.Errors;
 
@@ -24,6 +28,10 @@ public class PatientController {
 
 	@Autowired
 	private UserService userService;	
+	
+
+	@Autowired
+	private ReservationService reservationService;	
 		
    @GetMapping(value="/profile")
    public String admin (Model model) throws BusinessException
@@ -38,10 +46,31 @@ public class PatientController {
  
    @PostMapping(value="/profile")
    public String changeProfile (@Valid @ModelAttribute("user") User user, Errors errors ) throws BusinessException {
+<<<<<<< Updated upstream
 	   System.out.print(user.getRole());
+=======
+		User currentUser = userService.findUserById(user.getId());
+>>>>>>> Stashed changes
 	   if (!errors.hasErrors()) {
-		   userService.updateUser(user);
+		   currentUser.setEmail(user.getEmail());
+		   currentUser.setName(user.getName());
+		   currentUser.setSurname(user.getSurname());
+		   currentUser.setDateOfBirth(user.getDateOfBirth());
+		   userService.updateUser(currentUser);
 	   }
 	   return "redirect:/patient/profile";
    }
+   
+   @GetMapping(value="/reservations")
+   public String reservationList (Model model) throws BusinessException
+   {	
+	   Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();	
+		User user = userService.findUserByUsername(userDetails.getUsername());
+		List<Reservation> reservation = reservationService.findReservationsByPatient(user.getId());
+		model.addAttribute("reservations", reservation);
+		return "private/patient/dashboardMyListings"; 
+   }
+   
+   
 }
